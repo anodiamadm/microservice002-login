8f335a43339615e5f7575e58de9b6c3388411243
anodiamadm <anodiamadm@gmail.com>
2021-08-28 19:58:13 +1000

    Delete PermissionRepository.java

--- src/main/java/com/anodiam/LoginRESTAPI/db/PermissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.anodiam.LoginRESTAPI.db;
-
-import com.anodiam.LoginRESTAPI.model.Permission;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PermissionRepository extends JpaRepository<Permission, Long> {
-}
