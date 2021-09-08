b75d13d6070377d6b6c0d49ade6ac0c2d22d1ed0
anodiamadm <anodiamadm@gmail.com>
2021-09-08 11:40:24 +1000

    Update Permission.java

--- src/main/java/com/anodiam/LoginRESTAPI/model/Permission.java
+++ src/main/java/com/anodiam/LoginRESTAPI/model/Permission.java
@@ -1,25 +1,30 @@
 package com.anodiam.LoginRESTAPI.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+//import com.fasterxml.jackson.annotation.JsonBackReference;
+//import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collection;
 
 @Entity
-@Table(name = "mst_permission")
+@Table(name = "mst_permission",
+		uniqueConstraints={@UniqueConstraint(name="uk_permission_name", columnNames="permission_name")},
+		indexes={@Index(name="idx_permission_name", columnList="permission_name")})
 public class Permission {
-	
+
 	@Id
 	@Column(name = "permission_id", nullable = false, updatable = false)
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long permissionId;
+	private BigInteger permissionId;
+
+	@Column(name = "permission_name", nullable = false, updatable = false, length = 255)
 	private String permissionName;
 
-	@ManyToMany(mappedBy="permissionList")
-	@JsonBackReference
-	@JsonIgnore
+	@ManyToMany(mappedBy = "permissionList")
+//	@JsonBackReference
+//	@JsonIgnore
 	private Collection<User> userList = new ArrayList<>();
 
 	public Permission(String permissionName) {
@@ -29,7 +34,11 @@ public class Permission {
 	public Permission() {
 	}
 
-	public Long getPermissionId() {
+	public void setPermissionId(BigInteger permissionId) {
+		this.permissionId = permissionId;
+	}
+
+	public BigInteger getPermissionId() {
 		return permissionId;
 	}
 
@@ -41,8 +50,8 @@ public class Permission {
 		this.permissionName = permissionName;
 	}
 
-	@JsonBackReference
-	@JsonIgnore
+	//	@JsonBackReference
+//	@JsonIgnore
 	public Collection<User> getUserList() {
 		return userList;
 	}
