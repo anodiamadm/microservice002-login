15c7cd62d078c894b3c1acd63d3a3fdda014d778
anodiamadm <anodiamadm@gmail.com>
2021-09-08 11:40:32 +1000

    Update User.java

--- src/main/java/com/anodiam/LoginRESTAPI/model/User.java
+++ src/main/java/com/anodiam/LoginRESTAPI/model/User.java
@@ -1,42 +1,23 @@
 package com.anodiam.LoginRESTAPI.model;
 
-import com.fasterxml.jackson.annotation.JsonManagedReference;
 import org.hibernate.annotations.LazyCollection;
 import org.hibernate.annotations.LazyCollectionOption;
 
 import javax.persistence.*;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Entity
 @Table(name = "mst_user",
-        uniqueConstraints={@UniqueConstraint(name="uk_username", columnNames="username"),
-                @UniqueConstraint(name="uk_email", columnNames="email")},
-        indexes={@Index(name="idx_username", columnList="username"),
-                @Index(name="idx_first_name", columnList="first_name"),
-                @Index(name="idx_last_name", columnList="last_name"),
-                @Index(name="idx_email", columnList="email")})
+        uniqueConstraints={@UniqueConstraint(name="uk_username", columnNames="username")},
+        indexes={@Index(name="idx_username", columnList="username")})
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "user_id")
-    private Long userId;
-
-    @Column(nullable = false, name="first_name", length = 255)
-    private String firstName;
-
-    @Column(nullable = false, name="last_name", length = 255)
-    private String lastName;
-
-    @Column(nullable = false, length = 255)
-    private String username;
-
-    @Column(nullable = false, length = 255)
-    private String email;
-
-    @Column(nullable = false)
-    private String password;
+    @Column(name = "user_id", nullable = false)
+    private BigInteger userId;
 
     @Column(nullable = false)
     private int active;
@@ -44,45 +25,36 @@ public class User {
     @Column(nullable = false, name="date_created")
     private Date dateCreated;
 
+    @Column(nullable = false, length = 511)
+    private String password;
+
+    @Column(nullable = false, length = 255)
+    private String username;
+
     @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.MERGE)
     @LazyCollection(LazyCollectionOption.FALSE)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
-    @JsonManagedReference
+//    @JsonManagedReference
     private List<Role> roleList = new ArrayList<>();
 
     @ManyToMany(cascade = CascadeType.MERGE)
     @LazyCollection(LazyCollectionOption.FALSE)
     @JoinTable(name = "user_permission", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "permission_id"))
-    @JsonManagedReference
+//    @JsonManagedReference
     private List<Permission> permissionList = new ArrayList<>();
 
-    public User(String username, String firstName, String lastName, String email, String password) {
+    public User(String username, String password) {
         this.username = username;
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.email = email;
         this.password = password;
         this.active=1;
         this.dateCreated = new Date();
     }
 
+    public void setUserId(BigInteger userId) {
+        this.userId = userId;
+    }
+
     protected User(){}
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
 
     public Date getDateCreated() {
         return dateCreated;
@@ -92,20 +64,12 @@ public class User {
         this.dateCreated = dateCreated;
     }
 
-    public Long getUserId() {
+    public BigInteger getUserId() {
         return userId;
     }
 
     public String getUsername() {
         return username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public void setUsername(String username) {
