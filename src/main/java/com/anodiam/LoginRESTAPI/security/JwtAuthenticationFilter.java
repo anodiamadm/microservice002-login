199bd5fd18d20d6bc051a586c7889299871a6957
unknown <anodiam.adm@gmail.com>
2021-11-24 17:25:24 +1100

    User ID removed from Login microservice's returned JSON
    24/11/2021

--- src/main/java/com/anodiam/LoginRESTAPI/security/JwtAuthenticationFilter.java
+++ src/main/java/com/anodiam/LoginRESTAPI/security/JwtAuthenticationFilter.java
@@ -5,7 +5,7 @@ import com.anodiam.LoginRESTAPI.serviceRepository.User.GeneralEncoderDecoder;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.json.JSONObject;
+//import org.json.JSONObject;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -65,15 +65,16 @@ public class JwtAuthenticationFilter extends UsernamePasswordAuthenticationFilte
                 .sign(Algorithm.HMAC512(jwtProperties.getSECRET().getBytes()));
 
         // Add Token in response
-        JSONObject item = new JSONObject();
-        try
-        {
-            item.put("UserId", principal.getUser().getUserId());
-            item.put("Token", token);
-        }catch(Exception ex){
-            ex.printStackTrace();
-        }
-        response.getOutputStream().print(item.toString());
+        response.getOutputStream().print(token);
+//        JSONObject item = new JSONObject();
+//        try
+//        {
+//            item.put("UserId", principal.getUser().getUserId());
+//            item.put("Token", token);
+//        }catch(Exception ex){
+//            ex.printStackTrace();
+//        }
+//        response.getOutputStream().print(item.toString());
         response.flushBuffer();
     }
 }
