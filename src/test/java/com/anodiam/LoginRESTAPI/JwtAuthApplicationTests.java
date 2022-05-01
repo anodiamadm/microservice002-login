77f1f33ea14724661832653c5eab92093192c34c
unknown <anodiam.adm@gmail.com>
2022-05-01 21:58:42 +1000

    Update JwtAuthApplicationTests.java

--- src/test/java/com/anodiam/LoginRESTAPI/JwtAuthApplicationTests.java
+++ src/test/java/com/anodiam/LoginRESTAPI/JwtAuthApplicationTests.java
@@ -3,47 +3,52 @@ package com.anodiam.LoginRESTAPI;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.Assert.assertEquals;
+
 @SpringBootTest
 class JwtAuthApplicationTests {
 
 //*******************************************************************************************
-//	JUNIT TEST CASES for microservice002-login Test Based Development Starts Here
+//	NEEDS MANUAL UNIT TESTING.
+//	Because ONLY BEARER token and status code are returned. No message response to compare.
 //*******************************************************************************************
-//	Following JUnits need to pass at Dev environment to be deployed to Test environment
+//	DUMMY JUNIT TEST CASES for microservice002-login Test Based Development ALWAYS TO PASS.
+//*******************************************************************************************
 
 	@Test
 	public void testPositiveSuccessfulLogin() throws Exception
 	{
 		String userName="pinaki.sen@gmail.com";
-		String password="Pinaki@123";
+		String password="Pinaki@12";
 //		**************************************************
-//		**************************************************
-	}
-
-	@Test
-	public void testNegativeWrongUsernamePassword() throws Exception
-	{
-		String userName="abcdefgh";
-		String password="adcb@12AB";
-//		**************************************************
+		assertEquals("DUMMY JUNIT TEST","DUMMY JUNIT TEST");
 //		**************************************************
 	}
 
-	@Test
-	public void testNegativeWrongUsername() throws Exception
-	{
-		String userName="pinakidas";
-		String password="Pinaki@123";
-//		**************************************************
-//		**************************************************
-	}
-
-	@Test
-	public void testNegativeWrongPassword() throws Exception
-	{
-		String userName="pinakidas";
-		String password="abcdefgh@12AB";
-//		**************************************************
-//		**************************************************
-	}
+//	@Test
+//	public void testNegativeWrongUsernamePassword() throws Exception
+//	{
+//		String userName="abcdefgh";
+//		String password="adcb@12AB";
+////		**************************************************
+////		**************************************************
+//	}
+//
+//	@Test
+//	public void testNegativeWrongUsername() throws Exception
+//	{
+//		String userName="pinaki.sen";
+//		String password="Pinaki@12";
+////		**************************************************
+////		**************************************************
+//	}
+//
+//	@Test
+//	public void testNegativeWrongPassword() throws Exception
+//	{
+//		String userName="pinaki.sen@gmail.com";
+//		String password="abcdefgh@12AB";
+////		**************************************************
+////		**************************************************
+//	}
 }
